bb3d424763df5c7d21888f59f051c2bd3d0c6079
tcandens <joey.thies@gmail.com>
2017-10-07 18:55:17 -0700

    Initial server setup

---
+++ api/gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp')
+const ts = require('gulp-typescript')
+const rename = require('gulp-rename')
+const nodemon = require('gulp-nodemon')
+
+const tsProject = ts.createProject('tsconfig.json')
+
+gulp.task('build-ts', function() {
+  gulp.src('src/**/*.ts')
+    .pipe(tsProject())
+    .pipe(rename({
+      extname: '.mjs',
+    }))
+    .pipe(gulp.dest("build"))
+})
+
+gulp.task('dev', ['build-ts'], function() {
+  gulp.watch('src/**/*.ts', ['build-ts'])
+  nodemon({
+    script: 'build/index.mjs',
+    ext: 'mjs',
+    nodeArgs: ['--experimental-modules']
+  })
+})
