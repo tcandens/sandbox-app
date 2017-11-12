91eb64078932644ab2a7689179424110f102e6e7
tcandens <joey.thies@gmail.com>
2017-11-11 23:43:19 -0800

    Fix db name

--- api/src/connections/db.ts
+++ api/src/connections/db.ts
@@ -1,6 +1,6 @@
 import * as Sequelize from 'sequelize'
 
-const sequelize = new Sequelize('drainer', 'trainer', 'tr@1n3r', {
+const sequelize = new Sequelize('trainer', 'trainer', 'tr@1n3r', {
   host: 'db',
   dialect: 'postgres',
 })
