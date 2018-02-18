d8ba4dce57b057217beea3319412e678284e56f1
tcandens <joey.thies@gmail.com>
2018-02-17 19:52:05 -0800

    Working docker and nodejs inspector setup

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,4 +2,10 @@ import app from './app'
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT)
+app.listen(PORT, () => {
+  console.log('Listening on port ' + PORT)
+})
+
+app.on('error', error => {
+  console.dir(error)
+})
